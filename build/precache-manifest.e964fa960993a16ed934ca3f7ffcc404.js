11ecfc82dc564d6998ca26114dd537ff978e9c13
nikola djordjevic <nikola.dj.87@gmail.com>
2020-10-03 21:07:35 +0200

    first

---
+++ build/precache-manifest.e964fa960993a16ed934ca3f7ffcc404.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98bb17818b60cdaf7848add98722693",
+    "url": "/index.html"
+  },
+  {
+    "revision": "615c3a218785d03b86b0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9cffaa2e89fd9bee15c8",
+    "url": "/static/js/2.b7610edc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7610edc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615c3a218785d03b86b0",
+    "url": "/static/js/main.df22b7f0.chunk.js"
+  },
+  {
+    "revision": "24c06fb364d92fc74c37",
+    "url": "/static/js/runtime-main.8529c42a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
