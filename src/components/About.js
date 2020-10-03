4d6e30b7be3be9bb84b90c0221a8a6567f47b580
nikola djordjevic <nikola.dj.87@gmail.com>
2020-10-04 01:49:15 +0200

    about

--- src/components/About.js
+++ src/components/About.js
@@ -15,16 +15,52 @@ function About() {
         </div>
         <div className="mainStack">
           <h3 className="mainStackTitle">Preferred web stack:</h3>
+          {/* *******  */}
           <div className="stackItem">
             <img
               className="stackLinkImage"
               src="./images/icons/react.png"
               alt="react"
             />
-            <a href="#">
+            <a href="https://reactjs.org/" target="_blank">
               <p className="linkName">React</p>
             </a>
             <p className="linkNameSpan">- UI Library</p>
+          </div>
+          {/* ********** */}
+          <div className="stackItem">
+            <img
+              className="stackLinkImage"
+              src="./images/icons/nodejs.png"
+              alt="react"
+            />
+            <a href="https://nodejs.org/en/" target="_blank">
+              <p className="linkName">Node JS</p>
+            </a>
+            <p className="linkNameSpan">- JavaScript Runtime</p>
+          </div>
+          <div className="stackItem">
+            <img
+              className="stackLinkImage"
+              src="./images/icons/express.png"
+              alt="react"
+            />
+            <a href="https://expressjs.com/" target="_blank">
+              <p className="linkName">Express</p>
+            </a>
+            <p className="linkNameSpan">- Node.js Framework</p>
+          </div>
+          <h3 className="mainStackTitle">I also use:</h3>
+          <div className="stackItem">
+            <img
+              className="stackLinkImage"
+              src="./images/icons/mongoDB.png"
+              alt="react"
+            />
+            <a href="https://www.mongodb.com/" target="_blank">
+              <p className="linkName">MongoDB</p>
+            </a>
+            <p className="linkNameSpan">- NoSQL Database</p>
           </div>
         </div>
       </div>
